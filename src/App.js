4970e0bd6001ecfaa7037f673b3b35fe66ecf3e0
AnneKin <annekhd98@gmail.com>
2022-10-18 13:09:25 -0300

    app components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import HabitsPage from "./pages/HabitsPage/HabitsPage";
+import TodayPage from "./pages/TodayPage/TodayPage";
+import HistoryPage from "./pages/HistoryPage/HistoryPage";
+
 function App() {
   return (
-   <div>
-    TrackIt
-    </div>
-  )
+    <>
+      <LoginPage />
+      <SignUpPage />
+      <HabitsPage />
+      <TodayPage />
+      <HistoryPage />
+    </>
+  );
 }
 
 export default App;
