07ab57d798b48edfdc9fa82871b479d14082c48c
AnneKin <annekhd98@gmail.com>
2022-10-18 13:24:11 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import GlobalStyle from "./assets/styles/GlobalStyle"
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import HabitsPage from "./pages/HabitsPage/HabitsPage";
 import TodayPage from "./pages/TodayPage/TodayPage";
 import HistoryPage from "./pages/HistoryPage/HistoryPage";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <GlobalStyle />
       <LoginPage />
       <SignUpPage />
       <HabitsPage />
       <TodayPage />
       <HistoryPage />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+
+body{
+    background-color:lightblue;
+}
+
+
+`
+
+export default GlobalStyle
