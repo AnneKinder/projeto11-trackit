8b4b68f1625fbbec67b7bbc960f8eef7a350de38
AnneKin <annekhd98@gmail.com>
2022-10-19 21:30:34 -0300

    today header

--- src/pages/TodayPage/TodayBar.js
+++ src/pages/TodayPage/TodayBar.js
@@ -1,18 +1,54 @@
 import styled from "styled-components";
 
-
-
-
 export default function TodayBar() {
-    return (
-      <TodayBarSty>
-        <div> oi </div>
-      </TodayBarSty>
-    );
-  }
-  
+  const today = { day: "Segunda", date: "17/10" };
 
-  const TodayBarSty = styled.div`
-  background-color: lightgreen;
-  
-  `
+  return (
+    <TodayBarSty>
+      <h1>
+        {" "}
+        {today.day}, {today.date}
+      </h1>
+      <NothingSty>Nenhum hábito concluído ainda </NothingSty>
+      <SomeSty></SomeSty>
+    </TodayBarSty>
+  );
+}
+
+const TodayBarSty = styled.div`
+  box-sizing: border-box;
+  height: 70px;
+  width: 340px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 20px;
+  font-family: "Lexend Deca";
+
+  h1 {
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126ba5;
+  }
+`;
+
+const NothingSty = styled.p`
+  font-family: "Lexend Deca";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17.976px;
+  line-height: 22px;
+  color: #bababa;
+`;
+
+const SomeSty = styled.p`
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 17.976px;
+line-height: 22px;
+color: #8FC549;
+
+
+`;
