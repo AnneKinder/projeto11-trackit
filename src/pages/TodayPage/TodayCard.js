694316daf0736e7fa3e04183e19c0505a659e0ca
AnneKin <annekhd98@gmail.com>
2022-10-23 15:31:36 -0300

    uncheck api

--- src/pages/TodayPage/TodayCard.js
+++ src/pages/TodayPage/TodayCard.js
@@ -4,14 +4,13 @@ import { IonIcon } from "@ionic/react";
 import { useState } from "react";
 import { Checkbox } from "react-ionicons";
 import axios from "axios";
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
+
 
 export default function TodayCard(props) {
   const { name, sequence, record, id, isDone, token, checkHabits, setCheckHabits } = props;
-  let [boxColor, setBoxColor] = useState("#ebebeb");
   
-let body= {done:true}
-  //let id
+  let bodyCheck= {done:true}
+  let bodyUncheck={done:false}
 
   const config = {
     headers: {
@@ -22,15 +21,18 @@ let body= {done:true}
   function BoxAction() {
 
 
-    if (checkHabits.includes(id)) {
+    if (isDone===true) {
       alert("ja foi enviado"); // fazer requisiçao de delete aqui
-  
+   
+      axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`, bodyUncheck, config)
+      .then(console.log("unchecked"))
+      .catch((err) => console.log(err.data))
   
     } else {
 
       axios
         .post(
-          `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, body,
+          `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, bodyCheck,
           config
         )
         .then((res) => {
